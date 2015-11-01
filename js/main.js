4729b0224c81b6ef11d99ad1136d5602e51bbdf9
Kaiyin Zhong <kindlychung@gmail.com>
2015-11-02 00:14:26 +0100

    complete. chromosome visualization to be added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,60 @@
-var dataFile = "data/hg19.csv"
+
+var build = "hg19";
+var dataFile = "data/" + build + ".csv";
+
+$("#hgBuild").on("change", function () {
+    build = this.value;
+    dataFile = "data/" + build + ".csv";
+    updateCytoband();
+})
+
+var chrPosPattern = /(\d+|[xyXY]):(\d+)/g
+var userInput = "";
+
+var chrs = [];
+var positions = [];
+var interpreteInput = function () {
+    chrs = [];
+    positions = [];
+    while (m = chrPosPattern.exec(userInput)) {
+        var chr = m[1];
+        if (chr.toLowerCase() === "x" || chr.toLowerCase() == "y") {
+            chrs.push(chr.toUpperCase());
+            var pos = m[2];
+            positions.push(pos);
+        } else {
+            var chrInt = parseInt(chr);
+            if (chrInt < 1 || chrInt > 22) {
+                continue;
+            } else {
+                chrs.push(chr);
+                var pos = m[2];
+                positions.push(pos);
+            }
+        }
+    }
+    //console.log(chrs);
+    //console.log(positions);
+}
+
+
+$("#sendBtn").click(function() {
+    updateCytoband();
+});
+$("#chrPosInput").bind("keypress", function(event) {
+    if(event.which == 13) {
+        event.preventDefault();
+        updateCytoband();
+    }
+})
+
+var updateCytoband = function () {
+    $("#cytoTable").empty();
+    userInput = $("#chrPosInput").val()
+    interpreteInput();
+    getCytoBand(chrs, positions);
+}
+
 
 var getCytoBand = function (chrSymbol, basePosition) {
     d3.csv(dataFile, function (error, data) {
@@ -6,7 +62,6 @@ var getCytoBand = function (chrSymbol, basePosition) {
                 var res = data.filter(function (row) {
                     return row.chr === chrSymbol && +(row.start) <= basePosition && +(row.end) >= basePosition;
                 })
-                console.log(res);
                 return res;
             };
             var results = [];
@@ -18,7 +73,6 @@ var getCytoBand = function (chrSymbol, basePosition) {
             for (var i = 0; i < chrArray.length; i++) {
                 chrArray[i] = "chr" + chrArray[i];
             }
-            console.log(chrArray);
             if (!(basePosition instanceof Array)) {
                 var bpArray = [];
                 bpArray.push(parseInt(basePosition))
@@ -28,13 +82,12 @@ var getCytoBand = function (chrSymbol, basePosition) {
                     bpArray.push(parseInt(basePosition[i]))
                 }
             }
-            console.log(bpArray);
             var l1 = chrSymbol.length;
             var l2 = basePosition.length;
             var minLength = Math.min(l1, l2);
-            console.log(minLength);
             for (var i = 0; i < minLength; i++) {
                 var resOneBp = filterByOneBasePosition(chrSymbol[i], basePosition[i]);
+                console.log(resOneBp);
                 resOneBp = resOneBp[0];
                 resOneBp.pos = basePosition[i];
                 results.push([resOneBp["chr"], resOneBp["pos"], resOneBp["cyto"]]);
@@ -71,6 +124,12 @@ var getCytoBand = function (chrSymbol, basePosition) {
     );
 }
 
-//getCytoBand(22, 3840000);
-getCytoBand([22, 22, 13], [43214, 3840000, 43143214]);
+var popContent ="Chromosome and position seperated by ':'. If you have multiple queries, separate them by any non-digit character.</li>";
 
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+        title: "Input format",
+        content: popContent,
+        placement: "bottom",
+    });
+});
